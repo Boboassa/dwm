6df944505d707a1e5646d586d46a52d3311a6dfc
Boboassa <uca.baldassi99@gmail.com>
2022-04-29 14:41:21 +0200

    Migrate mappings to fzfwiki script
    Fix text alignment
    Add comments

--- commands.h
+++ commands.h
@@ -16,25 +16,35 @@ static const char *dmenucmd[] = {
     "-sf", selectedfg,
     NULL };
 
-static const char *termcmd[]   = { TERMINAL, NULL };
-static const char *editorcmd[] = { TERMINAL, "-e", "editor",  NULL };
-static const char *wikicmd[]   = { TERMINAL, "-e", "vim", "-c", "VimwikiIndex",  NULL };
-static const char *diaryedit[] = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "vwdiary", NULL };
-static const char *diaryview[] = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "vwdiary", "-v", NULL };
-static const char *calcmd[]    = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "calcurse", NULL };
+// General stuff
+static const char *termcmd[]     = { TERMINAL, NULL };
+//static const char *editorcmd[]   = { TERMINAL, "-e", "editor",  NULL };
+static const char *calcmd[]      = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "calcurse", NULL };
 
-static const char *browsercmd[] = { BROWSER, NULL };
+// Wiki interaction
+static const char *wikicmd[]     = { TERMINAL, "-e", "vim", "-c", "WikiIndex",  NULL };
+static const char *wikifzfedit[] = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "fzfwiki", NULL };
+// Append
+static const char *wikifzfapp[]  = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "fzfwiki", "-a", NULL };
+// Readonly
+static const char *wikifzfview[] = { TERMINAL, "-c", "float", "-g", "85x30", "-e", "fzfwiki", "-v", NULL };
 
-static const char *volup[] =          { "volume", "-i", NULL };
-static const char *voldown[] =        { "volume", "-d", NULL };
-static const char *togglemute[] =     { "volume", "-m", NULL };
+// Program launching
+static const char *browsercmd[]  = { BROWSER, NULL };
 
-static const char *dunstdel[] =       { "dunstctl", "close", NULL };
+// Volume control
+static const char *volup[]       =          { "volume", "-i", NULL };
+static const char *voldown[]     =        { "volume", "-d", NULL };
+static const char *togglemute[]  =     { "volume", "-m", NULL };
+
+// Notification dismiss
+static const char *dunstdel[]    =       { "dunstctl", "close", NULL };
 static const char *dunstdelall[] =    { "dunstctl", "close-all", NULL };
-static const char *dunstmute[] =      { "dunstctl", "set-paused", "toggle", NULL };
+static const char *dunstmute[]   =      { "dunstctl", "set-paused", "toggle", NULL };
 
-static const char *backlightup[] =    { "backlight", "-i", NULL };
-static const char *backlightdown[] =  { "backlight", "-d", NULL };
+// Backlight control
+static const char *blup[] =    { "backlight", "-i", NULL };
+static const char *bldown[] =  { "backlight", "-d", NULL };
 //static const char *backlightreset[] = { "backlight", "-r", NULL };
 
 static const char *launchmenu[] =     { "launch", "-o", NULL };
